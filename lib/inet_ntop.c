3c34b9da05f5bef4954c04d8ee6b9e171bfbef80
Henning Geinitz <sane@geinitz.org>
2002-01-06 20:43:10 +0000

    Added implementation of inet_ntop() for platforms withouth this function based on inet_ntoa. Henning Meier-Geinitz <henning@meier-geinitz.de>

---
+++ lib/inet_ntop.c
@@ -0,0 +1,32 @@
+#include "../include/sane/config.h"
+
+#ifndef HAVE_INET_NTOP
+
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+const char *
+inet_ntop (int af, const void *src, char *dst, size_t cnt)
+{
+  struct in_addr in;
+  char *text_addr;
+
+  if (af == AF_INET)
+    {
+      memcpy (&in.s_addr, src, sizeof (in.s_addr));
+      text_addr = inet_ntoa (in);
+      if (text_addr && dst)
+	{
+	  strncpy (dst, text_addr, cnt);
+	  return dst;
+	}
+      else
+	return 0;
+    }
+  return 0;
+}
+
+#endif /* !HAVE_INET_NTOP */

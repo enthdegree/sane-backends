2a44ef67eae5be2cb633e41d8a3a9305e6169305
Gerhard Jaeger <gerhard@gjaeger.de>
2002-01-17 19:48:38 +0000

    Updated files mainly because of the translation stuff and doxygen conform comments.

--- include/sane/sanei_lm983x.h
+++ include/sane/sanei_lm983x.h
@@ -41,9 +41,13 @@
    whether to permit this exception to apply to your modifications.
    If you do not wish that, delete this exception notice.
 
-   Interface files for the LM9831/2/3 chip,
-   a chip used in many USB scanners.
+*/
 
+/** @file sanei_lm983x.h
+ *  Interface files for the NS LM9831/2/3 USB chip.
+ *
+ *  The National Semiconductor LM9831, LM9832, and LM9833 chips are used in
+ *  many USB scanners. Examples include Plustek and Mustek devices.
  */
 
 #ifndef sanei_lm983x_h
@@ -52,22 +56,92 @@
 #include "../include/sane/config.h"
 #include "../include/sane/sane.h"
 
+/**
+ * Read one data byte from a specific LM983x register.
+ *
+ * @param fd    - device file descriptor (SANE_Int)
+ * @param reg   - number of register (SANE_Byte)
+ * @param value - byte value to be written (SANE_Byte *)
+ *
+ * @return The SANE status code for the operation (SANE_Status):
+ * - SANE_STATUS_GOOD     - on success
+ * - SANE_STATUS_IO_ERROR - system write function failed
+ * - SANE_STATUS_INVAL    - register out of range
+ */
 #define sanei_lm983x_read_byte(fd, reg, value) \
-                                       sanei_lm983x_read (fd, reg, value, 1, 0)
+          sanei_lm983x_read (fd, reg, value, 1, 0)
 
+/**
+ * Initialize sanei_lm983x.
+ *
+ * Currently, this function only enables the debugging functionality.
+ */
 extern void sanei_lm983x_init( void );
 
+/**
+ * Write one data byte to a specific LM983x register.
+ *
+ * @param fd    - device file descriptor
+ * @param reg   - number of register
+ * @param value - byte value to be written
+ *
+ * @return
+ * - SANE_STATUS_GOOD     - on success
+ * - SANE_STATUS_IO_ERROR - system write function failed
+ * - SANE_STATUS_INVAL    - register out of range
+ */
 extern SANE_Status sanei_lm983x_write_byte( SANE_Int fd,
                                             SANE_Byte reg, SANE_Byte value );
 
+/**
+ * Write one or more data bytes to one or more specific LM983x
+ * registers.
+ *
+ * @param fd        - device file descriptor
+ * @param reg       - number of start-register
+ * @param buffer    - buffer to be written
+ * @param len       - number of bytes to be written
+ * @param increment - SANE_TRUE enables the autoincrement of the register
+ *                    value during the write cycle, SANE_FALSE disables this
+ *
+ * @return
+ * - SANE_STATUS_GOOD     - on success
+ * - SANE_STATUS_IO_ERROR - system read function failed
+ * - SANE_STATUS_INVAL    - register out of range or len field was 0
+ */
 extern SANE_Status sanei_lm983x_write( SANE_Int fd, SANE_Byte reg,
                                        SANE_Byte *buffer, SANE_Word len,
                                                          SANE_Bool increment );
 
+/**
+ * Read one or more data bytes from one ore more specific LM983x
+ * registers.
+ *
+ * @param fd        - device file descriptor
+ * @param reg       - number of start-register
+ * @param buffer    - buffer to receive the data
+ * @param len       - number of bytes to receive
+ * @param increment - SANE_TRUE enables the autoincrement of the register
+ *                    value during the read cylce, SANE_FALSE disables this
+ *
+ * @return
+ * - SANE_STATUS_GOOD     - on success
+ * - SANE_STATUS_IO_ERROR - system read function failed
+ * - SANE_STATUS_INVAL    - register out of range
+ * - SANE_STATUS_EOF      - if nothing can't be read
+ */
 extern SANE_Status sanei_lm983x_read( SANE_Int fd, SANE_Byte reg,
                                       SANE_Byte *buffer, SANE_Word len,
                                                          SANE_Bool increment );
 
+/**
+ * Reset the LM983x chip.
+ *
+ * @param fd    - device file descriptor
+ * @return 
+ * - SANE_TRUE  - reset successfuly done
+ * - SANE_FALSE - reset failed
+ */
 extern SANE_Bool sanei_lm983x_reset( SANE_Int fd );
 					    					
 #endif /* sanei_lm983x_h */

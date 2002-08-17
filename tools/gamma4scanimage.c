790e5c9c25ff2a548ddb66037885495a5ee9bd28
Oliver Rauch <oliver.rauch@xsane.org>
2002-08-17 08:07:34 +0000

    2002-08-17 Oliver Rauch <Oliver.Rauch@rauch-domain.de>         * tools/gamma4scanimage.c: NEW: tool to create gamma table for scanimage         * tools/Makefile.in: changed file to compile and install gamma4scanimage

---
+++ tools/gamma4scanimage.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+ double gamma  = 1.0;
+ int shadow    = 0;
+ int highlight = 16383;
+ int maxin     = 16383;
+ int maxout    = 255;
+ int in, out;
+ float f;
+
+  if ( (argc==1) || (argc>6) )
+  {
+    printf("Usage: %s gamma [shadow [highlight [maxin [maxout]]]]\n", argv[0]);
+    exit(-1);
+  }
+
+  if (argc>1)
+  {
+    gamma     = atof(argv[1]);
+  }
+
+  if (argc>2)
+  {
+    shadow    = atof(argv[2]);
+  }
+
+  if (argc>3)
+  {
+    highlight = atof(argv[3]);
+  }
+
+  if (argc>4)
+  {
+    maxin     = atof(argv[4]);
+  }
+
+  if (argc>5)
+  {
+    maxout    = atof(argv[5]);
+  }
+
+  if (shadow < 0)
+  {
+    printf("%s error: shadow=%d < 0\n", argv[0], shadow);
+    exit(-1);
+  }
+
+  if (highlight < 0)
+  {
+    printf("%s error: highlight=%d < 0\n", argv[0], highlight);
+    exit(-1);
+  }
+
+  if (maxin < 0)
+  {
+    printf("%s error: maxin=%d < 0\n", argv[0], maxin);
+    exit(-1);
+  }
+
+  if (maxout < 0)
+  {
+    printf("%s error: maxout=%d < 0\n", argv[0], maxout);
+    exit(-1);
+  }
+
+  if (shadow >= highlight)
+  {
+    printf("%s error: shadow=%d >= highlight=%d\n", argv[0], shadow, highlight);
+    exit(-1);
+  }
+
+  if (highlight > maxin)
+  {
+    printf("%s error: highlight=%d > maxin=%d\n", argv[0], highlight, maxin);
+    exit(-1);
+  }
+
+  if ((gamma < 0.1) || (gamma > 5))
+  {
+    printf("%s error: gamma=%f out of range [0.1;5]\n", argv[0], gamma);
+    exit(-1);
+  }
+
+  f = (highlight - shadow) / 255 + shadow;
+
+  printf("[%d]%d-", 0, 0);
+
+  if (shadow > 0)
+  {
+    printf("[%d]%d-", shadow, 0);
+  }
+
+  while (f < highlight)
+  {
+    in = (int) f;
+    out = maxout * pow((double) (in - shadow)/(highlight-shadow), (1.0/gamma));
+    printf("[%d]%d-", in, out);
+    f *= 1.5;
+  }
+
+  if (f > highlight)
+  {
+    printf("[%d]%d-", highlight, maxout);
+  }
+
+  printf("[%d]%d", maxin, maxout);
+
+ return 0;
+}

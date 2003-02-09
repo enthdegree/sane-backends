b04729487fde760e510ca28bd66a88fbeed95fc8
Stéphane Voltz <stef.dev@free.fr>
2003-02-09 08:27:05 +0000

    - changed version number

--- tools/umax_pp.c
+++ tools/umax_pp.c
@@ -367,7 +367,7 @@ main (int argc, char **argv)
     }
   if (trace)
     {
-      printf ("UMAX 1220P scanning program version 2.17 starting ...\n");
+      printf ("UMAX 1220P scanning program version 3.0 starting ...\n");
 #ifdef HAVE_LINUX_PPDEV_H
       printf ("ppdev character device built-in.\n");
 #endif

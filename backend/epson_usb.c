170f42289e1505525d416e14300b8e4213a8d955
Karl Heinz Kremer <khk@khk.net>
2004-01-13 22:27:06 +0000

    Added product ID for RX-500

--- backend/epson_usb.c
+++ backend/epson_usb.c
@@ -21,6 +21,7 @@ SANE_Word sanei_epson_usb_product_ids [] =
 	0x11e, 	/* Perfection 1660 */
 	0x801,	/* CX-5200 */
 	0x802,	/* CX-3200 */
+	0x807,	/* RX-500 */
 	0	/* last entry - this is used for devices that are specified 
 		   in the config file as "usb <vendor> <product>" */
 };

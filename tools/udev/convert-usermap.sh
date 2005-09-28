7ed258567426acc2dc7bc3c99b2cc9c0a9d77daa
Julien BLACHE <jb@jblache.org>
2005-09-28 20:37:43 +0000

    tools/udev/convert-usermap.sh: Added a script to convert hotplug/libsane.usermap to a udev rules file. udev 070 + linux 2.6.14 will deprecate hotplug. tools/README: updated.

---
+++ tools/udev/convert-usermap.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Converts libsane.usermap to an udev rules file
+#
+
+if [ ! -e libsane.rules ]; then
+    cat > libsane.rules <<EOF
+# This file is part of the SANE distribution
+#
+# udev rules file for supported scanners
+#
+#
+# For now, only USB scanners are listed/supported by this set of rules;
+# feel free to add support for other busses.
+#
+# To add an USB scanner, add a rule to the list below between the SUBSYSTEM...
+# and LABEL... lines.
+#
+# To run a script when your scanner is plugged in, add RUN="/path/to/script"
+# to the appropriate rule.
+#
+
+SUBSYSTEM!="usb_device", ACTION!="add", GOTO="libsane_rules_end"
+
+EOF
+fi
+
+cat "$1" | { while read map; do
+    if $(echo "$map" | grep -q ^# > /dev/null); then
+	echo $map >> libsane.rules
+    else
+	set $map
+
+	echo -e "SYSFS{idVendor}==\"$3\", SYSFS{idProduct}==\"$4\", MODE=\"660\", GROUP=\"scanner\"" >> libsane.rules
+    fi
+done }
+
+echo >> libsane.rules
+echo "LABEL=\"libsane_rules_end\"" >> libsane.rules
+
+exit 0

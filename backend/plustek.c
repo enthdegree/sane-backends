716104d9609f60ede652ad32d5a642864059888b
Gerhard Jaeger <gerhard@gjaeger.de>
2005-10-02 19:36:15 +0000

    Bumped build number.

--- backend/plustek.c
+++ backend/plustek.c
@@ -153,7 +153,7 @@
 #include "../include/sane/sanei.h"
 #include "../include/sane/saneopts.h"
 
-#define BACKEND_VERSION "0.50-3"
+#define BACKEND_VERSION "0.50-4"
 
 #define BACKEND_NAME    plustek
 #include "../include/sane/sanei_access.h"

3b855e1b4bf7df30b97a20704424dc0796dbe291
Mattias Ellert <mattias.ellert@fysast.uu.se>
2008-02-20 17:27:53 +0000

    Device ID 0 is valid - check status code for errors instead

--- backend/sm3840.c
+++ backend/sm3840.c
@@ -805,10 +805,9 @@ sane_open (SANE_String_Const devicename, SANE_Handle * handle)
   if (!s)
     return SANE_STATUS_NO_MEM;
 
-  s->udev = 0;
   status = sanei_usb_open (dev->sane.name, &(s->udev));
-  if (!s->udev)
-    return SANE_STATUS_ACCESS_DENIED;
+  if (status != SANE_STATUS_GOOD)
+    return status;
 
   initialize_options_list (s);
   s->scanning = 0;

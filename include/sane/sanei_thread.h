0c8a3a0775b63271ef9006aab550957be780c9cd
m. allan noah <kitno455@gmail.com>
2009-01-20 01:41:14 +0000

    add MFC-7840W

--- include/sane/sanei_thread.h
+++ include/sane/sanei_thread.h
@@ -60,11 +60,7 @@
 #define sanei_thread_h
 #include "../include/sane/config.h"
 
-#ifdef USE_PTHREAD
-typedef long SANE_Pid;
-#else
 typedef int SANE_Pid;
-#endif
 
 /** Initialize sanei_thread.
  *

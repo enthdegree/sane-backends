7dab2ed86b4950b45b84c0c09623cdd7cbf2abda
m. allan noah <kitno455@gmail.com>
2009-05-26 08:53:02 -0400

    clamp film type to allowed range (bug #311685)

--- backend/canon.c
+++ backend/canon.c
@@ -253,6 +253,8 @@ get_tpu_stat (int fd, CANON_Device * dev)
   dev->tpu.Transparency = tbuf[4 + 4 + 5] * 256 + tbuf[5 + 4 + 5];
   dev->tpu.PosNeg = tbuf[6 + 4 + 5] & 0x01;
   dev->tpu.FilmType = tbuf[7 + 4 + 5];
+  if(dev->tpu.FilmType > 4)
+    dev->tpu.FilmType = 0;
 
   DBG (11, "TPU Status: %d\n", dev->tpu.Status);
   DBG (11, "TPU ControlMode: %d\n", dev->tpu.ControlMode);

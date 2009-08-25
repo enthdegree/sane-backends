4a6fc4312b7b50df2908fa5db58035efc14ab70f
Nicolas Martin <nicols-guest@users.alioth.debian.org>
2009-08-25 23:21:05 +0200

    For mf4660: try to change the read data command expected return len

--- backend/pixma_imageclass.c
+++ backend/pixma_imageclass.c
@@ -257,7 +257,7 @@ request_image_block (pixma_t * s, unsigned flag, uint8_t * info,
   pixma_set_be16 (cmd_read_image, mf->cb.buf);
   mf->cb.buf[8] = flag;
   mf->cb.buf[10] = 0x06;
-  mf->cb.reslen = pixma_cmd_transaction (s, mf->cb.buf, 11, mf->cb.buf, 8);
+  mf->cb.reslen = pixma_cmd_transaction (s, mf->cb.buf, 11, mf->cb.buf, 64);
   mf->cb.expected_reslen = 0;
   error = pixma_check_result (&mf->cb);
   if (error >= 0)

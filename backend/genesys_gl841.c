19d87357c6493a28f348b78e86df74495e34fd4e
Stéphane Voltz <stef.dev@free.fr>
2009-09-21 22:09:29 +0200

    honor half ccd flag in gl841_calulate_current_setup()

--- backend/genesys_gl841.c
+++ backend/genesys_gl841.c
@@ -3157,7 +3157,8 @@ gl841_calculate_current_setup (Genesys_Device * dev)
 
 /* half_ccd */
   /* we have 2 domains for ccd: xres below or above half ccd max dpi */
-  if (dev->sensor.optical_res  < 2 * xres) {
+  if ((dev->sensor.optical_res  < 2 * xres) ||
+     !(dev->model->flags & GENESYS_FLAG_HALF_CCD_MODE)) {
       half_ccd = SANE_FALSE;
   } else {
       half_ccd = SANE_TRUE;

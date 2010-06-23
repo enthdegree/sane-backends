ecf956fb5ddfa23ee40204f1f4648b3abdf40f9b
Julien BLACHE <jb@jblache.org>
2010-06-23 18:21:37 +0200

    kvs1025: fix missing prototype for kv_usb_escape()

--- backend/kvs1025_usb.h
+++ backend/kvs1025_usb.h
@@ -16,6 +16,11 @@ SANE_Bool kv_usb_already_open (PKV_DEV dev);
 void kv_usb_close (PKV_DEV dev);
 void kv_usb_cleanup (void);
 
+SANE_Status
+kv_usb_escape (PKV_DEV dev,
+	       PKV_CMD_HEADER header,
+	       unsigned char *status_byte);
+
 SANE_Status kv_usb_send_command (PKV_DEV dev,
 				 PKV_CMD_HEADER header,
 				 PKV_CMD_RESPONSE response);

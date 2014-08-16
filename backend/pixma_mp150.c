a3a2d2038dc8ae89cc715ccc1011110561fcb47d
Rolf Bensch <roben-guest@alioth.debian.org>
2014-08-16 21:56:46 +0200

    MX720 has button support

--- backend/pixma_mp150.c
+++ backend/pixma_mp150.c
@@ -934,6 +934,7 @@ handle_interrupt (pixma_t * s, int timeout)
   if (s->cfg->pid == MG6200_PID
       || s->cfg->pid == MG6300_PID
       || s->cfg->pid == MX520_PID
+      || s->cfg->pid == MX720_PID
       || s->cfg->pid == MX920_PID)
   /* button no. in buf[7]
    * size in buf[10] 01=A4; 02=Letter; 08=10x15; 09=13x18; 0b=auto

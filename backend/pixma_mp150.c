bf60fd1f61f6fa9349f1999a832eb7c586669309
Rolf Bensch <roben-guest@alioth.debian.org>
2015-02-21 10:34:52 +0100

    MG6400 doesn't need special image format post processing

--- backend/pixma_mp150.c
+++ backend/pixma_mp150.c
@@ -1149,6 +1149,7 @@ post_process_image_data (pixma_t * s, pixma_imagebuf_t * ib)
               && s->cfg->pid != MG5300_PID
               && s->cfg->pid != MG5500_PID
               && s->cfg->pid != MG6300_PID
+              && s->cfg->pid != MG6400_PID
               && s->cfg->pid != MG7100_PID)
               reorder_pixels (mp->linebuf, sptr, c, n, m, s->param->wx, line_size);
           

858e73da7eff0d05f092472211b9384ad353e134
Rolf Bensch <roben-guest@alioth.debian.org>
2015-04-03 19:09:34 +0200

    Pixma backend version 0.17.16

--- backend/pixma.h
+++ backend/pixma.h
@@ -114,7 +114,7 @@ typedef uint32_t uint32_t;
 /**@{*/
 #define PIXMA_VERSION_MAJOR 0
 #define PIXMA_VERSION_MINOR 17
-#define PIXMA_VERSION_BUILD 15
+#define PIXMA_VERSION_BUILD 16
 /**@}*/
 
 /** \name Error codes */

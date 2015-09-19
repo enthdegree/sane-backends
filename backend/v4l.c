9dd31f977c1a6ee36be551da967e911d52427a0e
Olaf Meeuwissen <paddy-hack@member.fsf.org>
2015-09-19 19:45:24 +0900

    Move test on first array element into the loop

--- backend/v4l.c
+++ backend/v4l.c
@@ -291,15 +291,11 @@ init_options (V4L_Scanner * s)
   if (!s->val[OPT_MODE].s)
     return SANE_STATUS_NO_MEM;
   s->opt[OPT_MODE].size = 1; // '\0'
-  if (mode_list[0] != 0)
+  for (i = 0; mode_list[i] != 0; ++i)
     {
-      s->opt[OPT_MODE].size = strlen(mode_list[0]) + 1;
-      for (i = 1; mode_list[i] != 0; ++i)
-        {
-          int len = strlen(mode_list[i]) + 1;
-          if (s->opt[OPT_MODE].size < len)
-              s->opt[OPT_MODE].size = len;
-        }
+      int len = strlen(mode_list[i]) + 1;
+      if (s->opt[OPT_MODE].size < len)
+        s->opt[OPT_MODE].size = len;
     }
 
   /* channel */
@@ -317,15 +313,11 @@ init_options (V4L_Scanner * s)
   if (s->channel[0] == 0 || s->channel[1] == 0)
     s->opt[OPT_CHANNEL].cap |= SANE_CAP_INACTIVE;
   s->opt[OPT_CHANNEL].size = 1; // '\0'
-  if (s->channel[0] != 0)
+  for (i = 0; s->channel[i] != 0; ++i)
     {
-      s->opt[OPT_CHANNEL].size = strlen(s->channel[0]) + 1;
-      for (i = 1; s->channel[i] != 0; ++i)
-        {
-          int len = strlen(s->channel[i]) + 1;
-          if (s->opt[OPT_CHANNEL].size < len)
-              s->opt[OPT_CHANNEL].size = len;
-        }
+      int len = strlen(s->channel[i]) + 1;
+      if (s->opt[OPT_CHANNEL].size < len)
+        s->opt[OPT_CHANNEL].size = len;
     }
 
   /* "Geometry" group: */

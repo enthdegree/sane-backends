2239d2aaff16964a5d67293f5e4220d235e2c84d
Alexander Hofmann <alexander.hofmann@new-h.de>
2015-10-06 17:42:05 +0200

    Replace obsolete interface with standard include files

--- sanei/sanei_ir.c
+++ sanei/sanei_ir.c
@@ -29,7 +29,8 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <values.h>
+#include <float.h>
+#include <limits.h>
 #include <math.h>
 
 #define BACKEND_NAME sanei_ir	/* name of this module for debugging */

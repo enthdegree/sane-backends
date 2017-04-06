f14285dba30404f23fc9de13d632e69f23f7cb75
Rolf Bensch <roben-guest@alioth.debian.org>
2017-04-06 11:20:40 +0200

    pixma_mp150: MG5400 Series doesn't need special image format post processing @ high dpi

--- backend/pixma_mp150.c
+++ backend/pixma_mp150.c
@@ -1170,6 +1170,7 @@ post_process_image_data (pixma_t * s, pixma_imagebuf_t * ib)
               && s->cfg->pid != MG3600_PID
               && s->cfg->pid != MG2100_PID
               && s->cfg->pid != MG5300_PID
+              && s->cfg->pid != MG5400_PID
               && s->cfg->pid != MG5500_PID
               && s->cfg->pid != MG6300_PID
               && s->cfg->pid != MG6400_PID

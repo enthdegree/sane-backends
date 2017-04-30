92e59791d2e478484409f5f2e634196863d7dafb
Olaf Meeuwissen <paddy-hack@member.fsf.org>
2017-04-30 20:05:12 +0900

    Fix [-Werror=implicit-function-declaration] compiler error
    The fileno() is only available if its feature test macro passes.  The
    various symbols are defined appropriately in ../include/sane/config.h
    during configuration of the build.

--- frontend/sicc.c
+++ frontend/sicc.c
@@ -15,6 +15,8 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
+#include "../include/sane/config.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/stat.h>

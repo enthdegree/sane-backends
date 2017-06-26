1a1c808c15c78ec63ecb66a9f4538ddb6d5a5761
Olaf Meeuwissen <paddy-hack@member.fsf.org>
2017-06-26 12:08:37 +0900

    plustek: Fix [-Wshift-negative-value] compiler warning

--- backend/plustek.c
+++ backend/plustek.c
@@ -239,9 +239,9 @@ static const SANE_String_Const source_list[] =
 
 static const SANE_Range percentage_range =
 {
-	-100 << SANE_FIXED_SCALE_SHIFT, /* minimum      */
-	 100 << SANE_FIXED_SCALE_SHIFT, /* maximum      */
-	   1 << SANE_FIXED_SCALE_SHIFT  /* quantization */
+	SANE_FIX(-100),         /* minimum      */
+	SANE_FIX( 100),         /* maximum      */
+	SANE_FIX(   1)          /* quantization */
 };
 
 static const SANE_Range warmup_range   = { -1,   999, 1 };

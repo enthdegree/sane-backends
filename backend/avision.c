c1c8ee8ea603652fee85c6dba824a82107e7ca7c
Michael Niewoehner <foss@mniewoehner.de>
2018-09-20 18:03:59 +0200

    backend/avision: i1120: Enable gray filter to match windows driver
    Signed-off-by: Michael Niewöhner <foss@mniewoehner.de>

--- backend/avision.c
+++ backend/avision.c
@@ -901,7 +901,7 @@ static Avision_HWEntry Avision_Device_List [] =
     { NULL, NULL,
       0x040a, 0x6013,
       "Kodak", "i1120",
-      AV_INT_BUTTON | AV_2ND_LINE_INTERLACED | AV_FORCE_CALIB },
+      AV_INT_BUTTON | AV_2ND_LINE_INTERLACED | AV_USE_GRAY_FILTER | AV_FORCE_CALIB },
       /* comment="duplex sheetfed scanner" */
       /* status="basic" */
       /* This is a Kodak OEM device manufactured by avision.

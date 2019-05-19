4c2c4eb6397b974c90a20c1f4f355730b700be27
Povilas Kanapickas <povilas@radix.lt>
2019-05-19 14:46:46 +0300

    genesys: Remove duplicate definition of DBGSTART and DBGCOMPLETED

--- backend/genesys_gl843.h
+++ backend/genesys_gl843.h
@@ -43,9 +43,6 @@
 
 #include "genesys.h"
 
-#define DBGSTART DBG (DBG_proc, "%s start\n", __func__);
-#define DBGCOMPLETED DBG (DBG_proc, "%s completed\n", __func__);
-
 #define REG01           0x01
 #define REG01_CISSET	0x80
 #define REG01_DOGENB	0x40

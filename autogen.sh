61bff8b60153f0189a9bcc5cb70d4a2b042b3f13
Povilas Kanapickas <povilas@radix.lt>
2019-05-25 09:12:18 +0300

    autotools: Restore patch to libtool to change libsane soname

--- autogen.sh
+++ autogen.sh
@@ -3,3 +3,4 @@ test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
 autoreconf --force --install --verbose --warnings=all "$srcdir"
+patch "$srcdir/ltmain.sh" "$srcdir/ltmain.sh.patch"

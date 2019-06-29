1017d790b61fce2944ea57f2fe4a1a767c09b505
Olaf Meeuwissen <paddy-hack@member.fsf.org>
2019-06-29 17:29:15 +0900

    Always use the latest upstream config.{guess,sub} in source tarballs
    This was one of the release tasks but we may as well find out as soon
    as possible whether these break anything ;-)

--- tools/update-upstreams.sh
+++ tools/update-upstreams.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -u
-# tools/update-upstream.sh -- files to their latest version
-# Copyright (C) 2017  Olaf Meeuwissen
+# tools/update-upstreams.sh -- files to their latest version
+# Copyright (C) 2017, 2019  Olaf Meeuwissen
 #
 # License: GPL-3.0+
 
 fetch () {
     if type curl 2>/dev/null >/dev/null ; then
-        curl --silent --remote-name $1
+        curl --silent --location --remote-name $1
         return
     fi
     if type wget 2>/dev/null >/dev/null ; then
@@ -14,7 +14,7 @@ fetch () {
     fi
 }
 
-CONFIG_BASE_URL=http://git.savannah.gnu.org/cgit/config.git/plain
+CONFIG_BASE_URL=https://git.savannah.gnu.org/cgit/config.git/plain
 
 for file in config.guess config.sub; do
     fetch $CONFIG_BASE_URL/$file

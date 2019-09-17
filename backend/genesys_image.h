93395bd24e21d9555fbe69d7bae89f9e22c49d33
Povilas Kanapickas <povilas@radix.lt>
2019-09-17 17:21:01 +0300

    genesys: Add wrapper to retrieve raw channel value from image

--- backend/genesys_image.h
+++ backend/genesys_image.h
@@ -65,6 +65,7 @@ public:
     void set_pixel(std::size_t x, std::size_t y, const Pixel& pixel);
 
     RawPixel get_raw_pixel(std::size_t x, std::size_t y) const;
+    std::uint16_t get_raw_channel(std::size_t x, std::size_t y, unsigned channel) const;
     void set_raw_pixel(std::size_t x, std::size_t y, const RawPixel& pixel);
 
     void resize(std::size_t width, std::size_t height, PixelFormat format);

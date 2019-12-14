355f2ddd4db6c51168f20111ffdafd0d836152ad
Povilas Kanapickas <povilas@radix.lt>
2019-12-14 10:11:35 +0200

    genesys: Fix black scans on HP 4850c

--- backend/genesys/tables_model.cpp
+++ backend/genesys/tables_model.cpp
@@ -268,7 +268,7 @@ void genesys_init_usb_device_tables()
     model.x_size = 219.6;
     model.y_size = 314.5;
 
-    model.y_offset_calib_white = 3.0;
+    model.y_offset_calib_white = 0.0;
     model.x_offset_calib_black = 0.0;
 
     model.x_offset_ta = 0.0;
@@ -295,8 +295,8 @@ void genesys_init_usb_device_tables()
     model.gpio_id = GpioId::G4050;
     model.motor_id = MotorId::G4050;
     model.flags = GENESYS_FLAG_OFFSET_CALIBRATION |
+                  GENESYS_FLAG_SHADING_REPARK |
                   GENESYS_FLAG_SKIP_WARMUP |
-                  GENESYS_FLAG_DARK_CALIBRATION |
                   GENESYS_FLAG_CUSTOM_GAMMA;
     model.buttons = GENESYS_HAS_SCAN_SW | GENESYS_HAS_FILE_SW | GENESYS_HAS_COPY_SW;
     model.shading_lines = 100;

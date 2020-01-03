b30784a73015fd00a5edf95628188f577158666a
Jakub Benda <jakub.benda@open.ac.uk>
2020-01-03 19:48:37 +0300

    xerox_mfp: Fix Samsung CLX-3175FW color scan "Not a JPEG file"
    Exclude "Samsung CLX-3170 Series" from JPEG mode. Closes #200.
    
    Reference: https://gitlab.com/sane-project/backends/issues/200

--- backend/xerox_mfp.c
+++ backend/xerox_mfp.c
@@ -212,6 +212,7 @@ static int isSupportedDevice(struct device __sane_unused__ *dev)
 	/* blacklist malfunctioning device(s) */
 	if (!strncmp(dev->sane.model, "SCX-4500W", 9) ||
             !strncmp(dev->sane.model, "C460", 4) ||
+	    !!strstr(dev->sane.model, "CLX-3170") ||
 	    !strncmp(dev->sane.model, "M288x", 5))
 	    return 0;
         return 1;

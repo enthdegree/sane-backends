60dc64f8f861ec400f2bb816a0df6b753a9dde06
Rolf Bensch <rolf@bensch-online.de>
2020-02-13 18:41:18 +0100

    pixma: backend version 0.27.1

--- backend/pixma/pixma.h
+++ backend/pixma/pixma.h
@@ -120,7 +120,7 @@ typedef uint32_t uint32_t;
 /**@{*/
 #define PIXMA_VERSION_MAJOR 0
 #define PIXMA_VERSION_MINOR 27
-#define PIXMA_VERSION_BUILD 0
+#define PIXMA_VERSION_BUILD 1
 /**@}*/
 
 /** \name Error codes */

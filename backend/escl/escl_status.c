c4d8cc424e8d9a21fefe5e00647ce2d9828f9e6a
Thierry HUCHARD <thierry@ordissimo.com>
2020-05-09 09:50:15 +0200

    Update Adf status.

--- backend/escl/escl_status.c
+++ backend/escl/escl_status.c
@@ -187,7 +187,10 @@ escl_status(const ESCL_Device *device, int source)
     /* Decode Job status */
     // Thank's Alexander Pevzner (pzz@apevzner.com)
     print_xml_platen_and_adf_status(node, &platen, &adf);
-    if (platen != SANE_STATUS_GOOD &&
+    if (platen == SANE_STATUS_DEVICE_BUSY &&
+        adf == SANE_STATUS_NO_DOCS) {
+        status = SANE_STATUS_NO_DOCS;
+    } else if (platen != SANE_STATUS_GOOD &&
         platen != SANE_STATUS_UNSUPPORTED) {
         status = platen;
     } else if (source == PLATEN) {
